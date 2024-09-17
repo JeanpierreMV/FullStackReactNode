df9487b8bb9bedbdad5262440c10e3ccc523af50
harolte <harolte@hotmail.com>
2024-09-16 22:39:11 -0500

    login

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { PawPrint } from 'lucide-react'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterClient from './pages/RegisterClient';
 import RegisterTipoMascota from './pages/RegisterTipoMascota';
 import RegisterMascota from './pages/RegisterMascota'; // Importa la nueva página
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         <Route path="/registrar-cliente" element={<RegisterClient />} />
         <Route path="/registrar-tipo-mascota" element={<RegisterTipoMascota />} />
         <Route path="/registrar-mascota" element={<RegisterMascota />} /> {/* Añade la nueva ruta */}
+        <Route path="/login" element={<Login />} /> {/* Añade la nueva ruta */}
       </Routes>
     </Router>
   );
